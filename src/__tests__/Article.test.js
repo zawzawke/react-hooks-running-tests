178b43c1bec9c435b4f8b3e0e1a8f7702273db35
Paul <paulallanmain@gmail.com>
2025-04-10 08:14:35 +0300

    Complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -3,9 +3,12 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 
 import Article from "../components/Article";
-
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,7 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      <p>please pass this test</p>    
     </div>
   );
 }
